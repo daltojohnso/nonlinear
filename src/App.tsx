7c506f9fc35b41d14a166adf6a5bc97840d6c28d
Dalton Johnson <dalton@pendo.io>
2020-01-17 07:43:15 -0800

    plot an equation

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Plot, {PlotParams} from 'react-plotly.js';
+import math from 'mathjs';
 
+const initialCandidate = 'x^2';
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [candidateEq, setCandidateEq] = useState<string>(initialCandidate);
+    const [equation, setEquation] = useState<string>(initialCandidate);
+
+    const plotlyProps = compileEquation(equation);
+
+    return (
+        <main>
+            <div>
+                f(x)=
+                <input
+                    type="text"
+                    value={candidateEq}
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>): void => setCandidateEq(e.target.value)} />
+                <button onClick={(): void => setEquation(candidateEq)}>plot</button>
+            </div>
+            <Plot
+                {...plotlyProps}
+            />
+        </main>
+    );
+};
+
+function compileEquation (eq: string, range = [-10, 10, 0.5]): PlotParams {
+    try {
+        const expr = math.compile(eq);
+        const [start, end, step] = range;
+        const xValues = math.range(start, end, step).toArray() as number[];
+        const yValues: number[] = xValues.map((x: number): number => expr.evaluate({x}));
+        return {
+            data: [{
+                x: xValues,
+                y: yValues,
+                type: 'scatter',
+                line: {
+                    shape: 'spline'
+                },
+                mode: 'lines'
+            }],
+            layout: {}
+        };
+    } catch {
+        return {
+            data: [{
+                x: [],
+                y: [],
+                type: 'scatter',
+                line: {
+                    shape: 'spline'
+                },
+                mode: 'lines'
+            }],
+            layout: {}
+        };
+    } finally {}
 }
 
 export default App;
